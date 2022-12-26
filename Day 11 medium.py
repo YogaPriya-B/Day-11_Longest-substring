f2c4aedc4943b495e93b861ce5f54c69f286a5a8
YogaPriya-B <101502505+YogaPriya-B@users.noreply.github.com>
2022-12-26 16:09:23 +0530

    Add files via upload

---
+++ Day 11 medium.py
@@ -0,0 +1,21 @@
+s="aaabbb"
+
+l1=[]
+
+a=0
+for i in range(0,len(s)+1):
+    for j in range(i+1,len(s)+1):
+      
+        l1.append(s[i:j])
+print(l1)
+k=3
+l=[]
+for m in l1:
+    n=set(m)
+    if(len(n)==k):
+        l.append(m)
+if(l==[]):
+    print("Not enough unique characters")
+else:
+  res=max(l,key=len)
+  print(res)
